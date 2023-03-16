61d23f33cc94754b5041d8291c5b417b646254ec
Ethan McRae <ethanmcrae0@gmail.com>
2023-03-16 14:20:40 -0600

    MVP functionality achieved

---
+++ server-http.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  const parsedUrl = url.parse(req.url, true);
+  if (parsedUrl.pathname === '/') {
+    const filePath = path.join(__dirname, 'index.html');
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.write('Internal server error');
+        res.end();
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        res.end();
+      }
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.write('Not found');
+    res.end();
+  }
+});
+
+module.exports = server;
---
+++ server-socket.js
@@ -0,0 +1,72 @@
+const WebSocket = require('ws');
+const net = require('net');
+const robot = require('robotjs');
+const translate = require('./lib/translate');
+
+const clients = {};
+const picoServer = net.createServer((socket) => {
+  // Remember the client
+  const clientId = socket.remoteAddress; // Generate a unique ID for the client
+  console.log(`Socket client ${clients[clientId] ? 're' : ''}connected: Pico`);
+  clients[clientId] = true; // Store the client ID in the object
+
+  socket.on('data', (data) => {
+    const msg = data.toString();
+    console.log('received data:', msg);
+    const res = translate(msg);
+    if (typeof res === 'string') robot.typeString(res);
+    else if (clients.HTTP) clients.HTTP.send(JSON.stringify(res));
+    else console.log('Could not send data to HTTP client');
+  });
+
+  socket.on('end', () => {
+    console.log('Socket client disconnected: Pico');
+  });
+
+  socket.on('error', (err) => {
+    if (err.errno === -54) // Connection reset (bad wifi connection)
+      console.log('Server/Pico connection lost because of bad wifi');
+    else
+      console.error('Server error:', err);
+  });
+});
+
+picoServer.on('error', (err) => {
+  console.error('Server error:', err);
+});
+
+/* ================================================================================ */
+/* ================================================================================ */
+/* ================================================================================ */
+/* ================================================================================ */
+/* ================================================================================ */
+
+const httpServer = new WebSocket.Server({ port: 3001 }, () => {
+  console.log('HTTP Websocket Server listening on port 3001')
+});
+
+httpServer.on('connection', (socket) => {
+  // Remember the client
+  const clientId = socket._socket.remoteAddress; // Generate a unique ID for the client
+  console.log(`Socket client ${clients[clientId] ? 're' : ''}connected: HTTP`);
+  clients[clientId] = socket; // Store the client ID in the object
+  clients.HTTP = socket;
+
+  socket.on('close', () => {
+    console.log('Socket client disconnected: HTTP');
+  });
+
+  socket.on('error', (err) => {
+    if (err.errno === -54) // Connection reset (bad wifi connection)
+      console.log('Server/HTTP connection lost because of bad wifi');
+    else
+      console.error('Server error:', err);
+  });
+});
+
+httpServer.on('error', (err) => {
+  console.error('Server error:', err);
+});
+
+
+module.exports = picoServer;
