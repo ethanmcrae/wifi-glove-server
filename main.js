035830e647f8286d73fa6df41bc2a64f8a171920
Ethan McRae <ethanmcrae0@gmail.com>
2023-03-16 14:45:36 -0600

    Adds TODOs

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ const httpServer = require('./server-http');
 const socketServer = require('./server-socket');
 
 httpServer.listen(8080, () => {
-  console.log('HTTP Server listening on port 8080');
+  console.log('HTTP Server listening on port 8080: http://localhost:8080');
 });
 
 socketServer.listen(3000, () => {
   // Command to determine public address: ifconfig | grep 'inet ' | grep -v 127.0.0.1
   console.log('Pico Websocket Server listening on port 3000\n');
 });
+
+/* TODO
+ * Try adding back the pico code that would scan the network
+ */
